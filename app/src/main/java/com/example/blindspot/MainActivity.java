86123ef5cb559d147d7e406f5a6c7de422d61f93
hanita <hhaller@wisc.edu>
2021-11-13 17:50:08 -0600

    Help page

--- app/src/main/java/com/example/blindspot/MainActivity.java
+++ app/src/main/java/com/example/blindspot/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.blindspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test
+
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 }
