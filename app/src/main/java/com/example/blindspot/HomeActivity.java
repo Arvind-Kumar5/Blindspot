13f514ab8c6804b7c438013e29f175d1e39ebe51
jannekebyck <jbyck@wisc.edu>
2021-12-01 17:26:13 -0600

    Home Screen

--- app/src/main/java/com/example/blindspot/HomeActivity.java
+++ app/src/main/java/com/example/blindspot/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.blindspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    public void home_to_settings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void home_to_obj(View view){
+        Intent intent = new Intent(this, ObjectDetection.class);
+        startActivity(intent);
+    }
+
 }
