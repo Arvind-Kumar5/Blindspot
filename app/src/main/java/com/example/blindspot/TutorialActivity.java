f94fb0d9788c2e61d215808e03aa3a1b837e9678
Arvind-Kumar5 <askumar3@wisc.edu>
2021-12-05 21:03:20 -0600

    Added text to speech in tutorial page.

--- app/src/main/java/com/example/blindspot/TutorialActivity.java
+++ app/src/main/java/com/example/blindspot/TutorialActivity.java
@@ -2,16 +2,68 @@ package com.example.blindspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.speech.tts.TextToSpeech;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class TutorialActivity extends AppCompatActivity {
+
+    private TextToSpeech mTTS;
+    private TextView tutorialText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        tutorialText = (TextView) findViewById(R.id.textView3);
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        tutorialText.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+        tutorialText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak();
+            }
+        });
+
+    }
+
+    private void speak(){
+        String text = tutorialText.getText().toString();
+
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
     }
 
     public void change_page(View view){
