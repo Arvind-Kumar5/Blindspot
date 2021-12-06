d6cf3d615525828b2d91cd8ac9560dd3b1ec6e2a
Arvind-Kumar5 <askumar3@wisc.edu>
2021-12-05 21:38:00 -0600

    Added speech to help page

--- app/src/main/java/com/example/blindspot/HelpActivity.java
+++ app/src/main/java/com/example/blindspot/HelpActivity.java
@@ -4,17 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class HelpActivity extends AppCompatActivity {
 
     TextView faqs;
+    private TextToSpeech mTTS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        mTTS.speak("The help page has an FAQ and a button to go back to the tutorial.", TextToSpeech.QUEUE_FLUSH, null, null);
+                        faqs.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
 
         String questions = "What is currently here?:\n" +
                 "I am going to type these questions" +
@@ -26,6 +50,19 @@ public class HelpActivity extends AppCompatActivity {
                 "in the fall of 2021. This is our final project\n\n";
         faqs = (TextView) findViewById(R.id.faqs);
         faqs.setText(questions);
+        faqs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                faq();
+            }
+        });
+
+    }
+
+    private void faq(){
+        String text = faqs.getText().toString();
+
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
     }
 
     // go to home page
--- app/src/main/java/com/example/blindspot/SettingsActivity.java
+++ app/src/main/java/com/example/blindspot/SettingsActivity.java
@@ -7,10 +7,16 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
 
+import java.util.Locale;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private TextToSpeech mTTS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,27 @@ public class SettingsActivity extends AppCompatActivity {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
             }
         });
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        String text = "On the settings page, you can go to accessibility, help, or you can change the volume of the app." +
+                                ".The home button is on the top left, the volume button is below the home button, the accessibility is below the volume button, and" +
+                                ".The help option is below the accessibility icon";
+                        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
     }
 
     public void home(View view){
@@ -46,12 +73,14 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void toAccessibility (View view) {
+        mTTS.speak("You clicked on accessibility.", TextToSpeech.QUEUE_FLUSH, null,null);
         Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
     public void toHelp (View view) {
+        mTTS.speak("You clicked on help.", TextToSpeech.QUEUE_FLUSH, null,null);
         Intent intent = new Intent(this, HelpActivity.class);
         startActivity(intent);
     }
