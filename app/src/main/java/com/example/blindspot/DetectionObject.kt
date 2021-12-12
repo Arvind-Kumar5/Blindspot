d83dc5135bd68ccc909590a1294f8b4313fc02d8
Arvind-Kumar5 <askumar3@wisc.edu>
2021-12-12 16:44:29 -0600

    Added credit to page with methods for object detection.

--- app/src/main/java/com/example/blindspot/DetectionObject.kt
+++ app/src/main/java/com/example/blindspot/DetectionObject.kt
@@ -1,3 +1,4 @@
+//MainActivity.kt, OverlaySurfaceView.kt, ObjectDetector.kt, YuvToRgbConverter.kt, and DetectionObject.kt are all implementations taken from https://linuxtut.com/en/fc661a72d08f7f59cf41/
 package com.example.blindspot
 
 import android.graphics.RectF
